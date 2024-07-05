2b295711d9edb139361a7f101f706a5dee56a64b
veniapputrii <veniapputrii@gmail.com>
2024-07-05 18:01:30 +0700

    v1.1

---
+++ 5. None.py
@@ -0,0 +1,10 @@
+#None is no null value in Python.
+# The None is an object that represents the absence of a value.
+#it is like an empty object
+
+age = 98
+if age is None :
+    print ("Invalid result")
+
+# ✨ what does it mean of string in python ("")? ✨
+# String is used for representing textual data => ('') / ("")
