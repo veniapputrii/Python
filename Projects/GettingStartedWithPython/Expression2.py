0f724e54d0f5d3d7fab83be42024c63cf7451eea
veniapputrii <veniapputrii@gmail.com>
2024-09-16 09:52:12 +0700

    exercise

---
+++ Projects/GettingStartedWithPython/Expression2.py
@@ -0,0 +1,19 @@
+#I'm still not entirely sure with the expression part. 
+#here I looked up another similar exercise for increasing my understanding
+
+#Exercise:
+
+#You need to write a discount program for a grocery store with the following conditions:
+
+#If a customer spends more than 300,000, they will receive a 15% discount.
+#A customer named Alya has spent 450,000.
+
+#Hint:
+
+#Remember, you're looking for the total amount to be paid 
+#after the discount is applied, not just the discount amount.
+
+# Don't change this code
+alya = 450000
+
+#fix it here
